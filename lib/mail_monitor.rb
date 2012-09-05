5c21c63d96a1e8386222a92a4640ac5fdec1e1a7
Kurt Robert Rudolph <rudolph.kurt@rudyindustries.com>
2012-09-05 13:29:31 -0500

    added a require statement for application

--- lib/mail_monitor.rb
+++ lib/mail_monitor.rb
@@ -1,7 +1,12 @@
 require 'rubygems'
 require 'mail'
+
+
+require 'mail_monitor/applicaion'
 require 'mail_monitor/monitor'
 require 'mail_monitor/mail_monitor_module'
+
+require 'mail_monitor/version'
 
 
 
