bcdb0383555d6d411085fe87e8ffaba725a59bf9
Kurt Robert Rudolph <rudolph.kurt@rudyindustries.com>
2012-09-05 13:27:52 -0500

    added an application file

---
+++ lib/mail_monitor/application.rb
@@ -0,0 +1,15 @@
+require 'shellwords'
+require 'optparse'
+
+module MailMonitor
+
+  ######################################################################
+  # MailMonitor main application object.  When invoking +mail_monitor+ from the
+  # command line, a MailMonitor::Application object is created and run.
+  #
+  class Application
+
+    def run
+    end
+
+  end
