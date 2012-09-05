2b5fbdcd8e126a7f4bd5d59fef7fdc2c2217abe2
Kurt Robert Rudolph <rudolph.kurt@rudyindustries.com>
2012-09-05 10:47:21 -0500

    modified the name of the file

--- lib/mail_monitor.rb
+++ lib/mail_monitor.rb
@@ -1,28 +1,37 @@
 #!/usr/bin/env ruby
-
+require 'rubygems'
 require 'mail'
 
-TIME_FREQUENCY = ARGV[0].to_f;
+#TIME_FREQUENCY = ARGV[0].to_f;
 
-notify_emails = ARGV.values_at(1...-1) 
-
-
+#notify_emails = ARGV[1].split(';')
 
 
 Mail.defaults do
-  retriever_method :pop3, :address    => "pop.gmail.com",
-                          :port       => 995,
-                          :user_name  => 'larry.testone',
-                          :password   => 'NzoDUsi89lMF',
-                          :enable_ssl => true
+  delivery_method :smtp, { :address              => 'smtp.gmail.com',
+                           :port                 => 587,
+                           :domain               => 'gmail.com',
+                           :user_name            => 'larry.testone@gmail.com',
+                           :password             => 'NzoDUsi89lMF',
+                           :authentication       => 'plain',
+                           :enable_starttls_auto => true }
+  retriever_method :imap, { :address    => 'imap.googlemail.com',
+                            :port       => 993,
+                            :user_name  => 'larry.testone@gmail.com',
+                            :password   => 'NzoDUsi89lMF',
+                            :enable_ssl => true }
 end
 
+puts Mail.last.subject
+
 Mail.deliver do
-   from    'larry.testone@gmail.com'
-   to      'larry.testone@gmail.com'
-   subject 'Notification'
-   body    'body'
+  to 'larry.testone@gmail.com'
+  from 'larry.testone@gmail.com'
+  subject 'testing sendmail'
+  body 'testing sendmail'
 end
+
+
 
 =begin
 while true do
