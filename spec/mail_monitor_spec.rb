5dc8bb4f40b2d7b9b31a5d4788734ee3d67e1ca7
Kurt Robert Rudolph <rudolph.kurt@rudyindustries.com>
2012-09-10 22:04:29 -0500

    wrote a trivial test

--- spec/mail_monitor_spec.rb
+++ spec/mail_monitor_spec.rb
@@ -1,5 +1,14 @@
-describe "RSpec Greeter" do
-  it "should grab a message from the inbox" do
-    puts 'wow'
+require_relative 'spec_helper'
+
+describe MailMonitor::Polling do
+  before(:each) do
+    include_relative 'fixtures/mail_defaults'
+    @polling = MailMonitor::Polling.new 10, Mail, Mail.new
   end 
-end
+    
+  it "It should send detect a fault and send out a notification" do
+    @polling.start
+    sleep (4)
+    @polling.stop
+  end
+end 
