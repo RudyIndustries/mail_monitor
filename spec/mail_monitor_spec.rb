fbac98a5ed9bcddb518909762b12fa7cf4e3d32a
Kurt Robert Rudolph <rudolph.kurt@rudyindustries.com>
2012-09-11 15:18:51 -0500

    added a few basic tests, 2nd currently failing

--- spec/mail_monitor_spec.rb
+++ spec/mail_monitor_spec.rb
@@ -1,14 +1,27 @@
 require_relative 'spec_helper'
+require 'pry'
 
 describe MailMonitor::Polling do
   before(:each) do
     include_relative 'fixtures/mail_defaults'
-    @polling = MailMonitor::Polling.new 10, Mail, Mail.new
+    @polling_frequency = 4
+    message = MailMonitor::Notifier.new do
+      to 'larry.testone@gmail.com'
+      from 'yo'
+      subject 'test'
+      body 'test'
+    end
+    @polling = MailMonitor::Polling.new @polling_frequency, MailMonitor::Retriever.new, message
   end 
     
-  it "It should send detect a fault and send out a notification" do
+  it "should start and stop the polling before the end of @polling_frequency" do
     @polling.start
-    sleep (4)
+    sleep (@polling_frequency / 2)
+    @polling.stop
+  end
+  it "should detect a fault" do
+    @polling.start
+    sleep (@polling_frequency * 2)
     @polling.stop
   end
 end 
